f6e36ad5cbd5cb1a3d61d11839bb3b38aaad7d67
Your Name <you@example.com>
2024-07-31 15:51:20 +0200

    Refactored tests to use different contexts. Added context setting method to Engine. Added result and nested result initialization to Engine.

--- tests/Engine.test.js
+++ tests/Engine.test.js
@@ -4,100 +4,102 @@ import {
 import assert from 'assert';
 
 describe('Engine.process', () => {
-
-    const context = {
-        a: {
-            type: "b"
+    const engine = new Engine({
+        init: function (){
+            return [];
         },
-        b: {
-            type: "c"
-        },
-        c: {
-            type: "Object",
-            items: {
-                x: {
-                    type: "d"
-                },
-                y: {
-                    type: "e"
-                }
-            }
-        },
-        d: {
-            type: "String"
-        },
-        e: {
-            type: "Object",
-            items: {
-                p: {
-                    type: "d"
-                }
-            }
-        },
-        f: {
-            type: "Array",
-            items: {
-                type: "e"
-            }
-        },
-        g: {
-            type: "g"
-        },
-        h: {
-            type: "Object",
-            items: {
-                i: {
-                    type: "h"
-                }
-            }
-        }
-    };
-    const engine = new Engine(
-        context,
-        function (value, type, definition, result){
+        task: function ({value, type, definition, result}){
             result.push(type);
         },
-        function (property, value, type, definition, result, nestedResult, taskResult){
+        nestedTask: function ({property, value, type, definition, result, nestedResult, taskResult}){
             result.push([property, nestedResult]);
         }
-    );
+    });
 
     it('should process native types', () => {
-        const result = engine.process("x", "String", []);
+        engine.context({});
+        const result = engine.process("x", "String");
         assert.deepEqual(result, ["String"]);
     });
 
     it('should process type chains', () => {
-        const result = engine.process("x", "d", []);
-        assert.deepEqual(result, ["d", "String"]);
+        engine.context({
+            s: {
+                type: "String"
+            }
+        });
+        const result = engine.process("x", "s");
+        assert.deepEqual(result, ["s", "String"]);
     });
 
     it('should process object properties', () => {
-        const result = engine.process({items: {p:"x"}}, "e", []);
-        assert.deepEqual(result, ["e", "Object", ["p", ["d", "String"]]]);
+        engine.context({
+            s: {type: "String"},
+            o: {
+                type: "Object",
+                items: {p: {type: "s"}}
+            }
+        });
+        const result = engine.process({items: {p:"x"}}, "o");
+        assert.deepEqual(result, ["o", "Object", ["p", ["s", "String"]]]);
     });
 
     it('should process nested objects', () => {
+        engine.context({
+            o1: {type: "o2"},
+            o2: {type: "o3"},
+            o3: {
+                type: "Object",
+                items: {
+                    x: {type: "s"},
+                    y: {type: "oo"}
+                }
+            },
+            s: {type: "String"},
+            oo: {
+                type: "Object",
+                items: {
+                    p: {type: "s"}
+                }
+            }
+        });
         const result = engine.process({
             items: {
                 x: "x", 
                 y: {
-                    items: {
-                        p:"y"
-                    }
+                    items: {p:"y"}
                 }
             }
-        }, "a", []);
+        }, "o1");
         assert.deepEqual(result, [
-            "a", "b", "c", "Object", 
-            ["x", ["d", "String"]],
-            ["y", ["e", "Object",
-                ["p", ["d", "String"]]
+            "o1", "o2", "o3", "Object", 
+            ["x", ["s", "String"]],
+            ["y", ["oo", "Object",
+                ["p", ["s", "String"]]
             ]]
         ]);
     });
 
     it('should process arrays', () => {
+        engine.context({
+            s: {
+                type: "String"
+            },
+            o: {
+                type: "Object",
+                items: {
+                    p: {
+                        type: "s"
+                    }
+                }
+            },
+            a: {
+                type: "Array",
+                items: {
+                    type: "o"
+                }
+            }
+        });
         const result = engine.process({
             items: [
                 {
@@ -111,32 +113,52 @@ describe('Engine.process', () => {
                     }
                 }
             ]
-        }, "f", []);
+        }, "a");
         assert.deepEqual(result, [
-            "f", "Array", 
-            [0, ["e", "Object",
-                ["p", ["d", "String"]]
+            "a", "Array", 
+            [0, ["o", "Object",
+                ["p", ["s", "String"]]
             ]],
-            [1, ["e", "Object",
-                ["p", ["d", "String"]]
+            [1, ["o", "Object",
+                ["p", ["s", "String"]]
             ]]
         ]);
     });
 
     it('should prevent infinite loops by simple variables', () => {
-        assert.throws(() => engine.process("x", "g", []));
+        engine.context({
+            l: {
+                type: "l"
+            }
+        });
+        assert.throws(() => engine.process("x", "l"));
     });
 
-    it('should not prevent infinite loops by properties', () => {
+    it('should not prevent infinite loops by object properties', () => {
+        engine.context({
+            l: {
+                type: "Object",
+                items: {
+                    p: {
+                        type: "l"
+                    }
+                }
+            }
+        });
         const result = engine.process({
             items: {
-                i: {}
+                p: {
+                    items: {
+                        p: {}
+                    }
+                }
             }
-        }, "h", []);
-        assert.deepEqual(result, [
-            "h", "Object",
-            [
-                "i", ["h", "Object"]
+        }, "l");
+        assert.deepEqual(result, 
+            ["l", "Object", [
+                "p", ["l", "Object", [
+                    "p", ["l", "Object"]
+                ]]
             ]
         ]);
     });
