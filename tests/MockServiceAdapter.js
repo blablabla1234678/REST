8445b7c1a204ee30fd2aa15bc7d2688c554166e9
Your Name <you@example.com>
2024-08-13 11:34:42 +0200

    Moved the URI processing to a separate UriTemplate class. This is the first step to support express. #11

--- tests/MockServiceAdapter.js
+++ tests/MockServiceAdapter.js
@@ -4,48 +4,27 @@ export default class MockServiceAdapter {
         this.routes = {};
     }
     add(method, uriTemplate, callback){
-        const key = method.toUpperCase()+" "+uriTemplate;
+        const key = method.toUpperCase()+" "+uriTemplate.string();
         this.routes[key] = callback;
-        const parameterPattern = /\{(\w+)\}/img;
-        const groups = [];
-        let questionMarkPosition = uriTemplate.indexOf("?");
-        if (questionMarkPosition === -1)
-            questionMarkPosition = uriTemplate.length;
-        const pattern = uriTemplate.replace(parameterPattern, (match, parameter, offset) => {
-            groups.push(parameter);
-            if (offset < questionMarkPosition)
-                return "(\\w+)";
-            else
-                return parameter + "=(\\w+)"
-        }).replace(/([\/\?])/img, "\\$1");
-        const regex = new RegExp("^"+pattern+"$", "im");
-        this.uriTemplates[uriTemplate] = {regex,groups};
+        this.uriTemplates[uriTemplate.string()] = uriTemplate;
     }
     handle(request){
         const method = request.method.toUpperCase();
+        let params;
         let actualTemplate;
-        let actualRegex;
-        let actualGroups;
         for (let uriTemplate in this.uriTemplates){
-            let {regex,groups} = this.uriTemplates[uriTemplate];
-            if (regex.test(request.uri)){
-                actualTemplate = uriTemplate;
-                actualRegex = regex;
-                actualGroups = groups;
+            actualTemplate = this.uriTemplates[uriTemplate];
+            params = actualTemplate.match(request.uri);
+            if (params)
                 break;
-            }
         }
         if (!actualTemplate)
             throw new Error('404 - Not found: ' + method + " " + request.uri);
-        const key = method + " " + actualTemplate;
+        const key = method + " " + actualTemplate.string();
         const callback = this.routes[key];
         if (!callback)
             throw new Error('404 - No callback for route: '+ method + " " + request.uri);
-        const params = Array.from(request.uri.match(actualRegex)).slice(1, actualGroups.length+1);
-        const paramsObject = {};
-        for (let index in actualGroups)
-            paramsObject[actualGroups[index]] = params[index];
-        const response = callback(request, paramsObject);
+        const response = callback(request, params);
         return response;
     }
 }
